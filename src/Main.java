91f569ac7289e0ea7f6979179a2f05a66107a6de
Sheaf-Morrison, James Lane <jlanesm@gmail.com>
2020-09-30 19:54:45 +1300

    Reads in the link lengths and max connections.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -11,7 +14,35 @@ public class Main {
     static int numberOfUniqueLinks;
     static int maxConnection;
     static int[] linkLengths;
-    static double preserve;
+
+    /**
+     * Read in the link lengths from the csv file
+     * @param filePath to the csv file
+     * @return an int array of each possible links' length
+     */
+    private static int[] readCSV(String filePath) {
+        int[] links = null;
+
+        // Parse CSV file into BufferedReader
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+            String line = br.readLine();
+            String[] split = line.split(",");
+            links = new int[split.length];
+            for (int i = 0; i < split.length; i++) {
+                links[i] = Integer.parseInt(split[i]);
+            }
+        } catch (FileNotFoundException e) {
+            //TODO
+            e.printStackTrace();
+        } catch (IOException e) {
+            //TODO
+            e.printStackTrace();
+        }
+
+        return links;
+    }
 
     /**
      * Clever method to get starting population
@@ -358,9 +389,10 @@ public class Main {
     /**
      * Restart the population
      * @param pop the stagnated population
+     * @param preserve percent for a solutions from the current population to be put into thr nre population
      * @return a new population
      */
-    private static Population RestartPopulation(Population pop){
+    private static Population RestartPopulation(Population pop, double preserve){
         Population newpop = new Population(pop.Size());
         List<Integer>[] currPheno = ConvertPopToPhenotype(pop);
         // preserve is the percent of the solution to keep when restarting
@@ -389,41 +421,66 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
-        //TODO
-        // Read in these parameters
+        // Set default parameters
         int popSize = 4;
         int numParents = 2;
+        double preservePercent = 0.2;
         double mutatePercent = 0.2;
-        numberOfNodes = 4;
-        numberOfUniqueLinks = ( numberOfNodes * (numberOfNodes - 1) ) / 2;
-        maxConnection = 2;
-        linkLengths = new int[numberOfUniqueLinks];
-        for (int i = 0; i < numberOfUniqueLinks; i++) {
-            linkLengths[i] = i + 1;
-        }
+        boolean plusInsteadOfComma = true;
 
-        preserve = 0.2; // This is the only optional parameter that can have a default value
+        try {
+            // Read in the parameters
+            if (args.length > 1) {
+                // First argument should be the filepath of the lengths to connect each node in a specific csv format
+                String filePath = args[0];
+                linkLengths = readCSV(filePath);
+                // Second argument should be the max number of connections for each node
+                maxConnection = Integer.parseInt(args[1]);
 
-        // Initialise starting population
-        Population pop = GenerateInitialPopulation(popSize);
-        do{
-            // Apply recombination, mutation
-            Population newpop = GenerateNewPopulation(pop, numParents, mutatePercent);
-            // Select a subset of newpop for the next pop
-            pop = UpdatePopulationPlus(pop, newpop);
-            // Decide if we need to restart due to stagnation
-            if(pop.hasConverged()){
-                pop = RestartPopulation(pop);
+                if (args.length > 2) {
+                    //TODO
+                    // Optional -Pop argument followed by an integer is the population size
+                    // Optional -parents argument followed by an integer is the number of parents
+                    // Optional -preserve argument followed by a double is the preserve Percent
+                    // Optional -mutate argument followed by a double is the mutatePercent
+                    // Optional argument of a plus or comma keyword to select the recombination method
+                }
             }
-        } while(!TerminationCriterion());
+            // If no parameter given then use hard coded test parameters
+            else {
 
-        //TODO
-        // Output result
-        List<Integer>[] popPheno = ConvertPopToPhenotype(pop);
-        int bestSolution = FindBestSolution(popPheno);
-        System.out.println("Genotype: " + pop.getSolution(bestSolution).toString());
-        System.out.println("Phenotype: " + popPheno[bestSolution].toString());
-        System.out.println("Score: " + Evaluate(popPheno[bestSolution]));
+                numberOfUniqueLinks = (numberOfNodes * (numberOfNodes - 1)) / 2;
+                linkLengths = new int[numberOfUniqueLinks];
+                for (int i = 0; i < numberOfUniqueLinks; i++) {
+                    linkLengths[i] = i + 1;
+                }
+            }
+
+
+            // Initialise starting population
+            Population pop = GenerateInitialPopulation(popSize);
+            do {
+                // Apply recombination, mutation
+                Population newpop = GenerateNewPopulation(pop, numParents, mutatePercent);
+                // Select a subset of newpop for the next pop
+                pop = UpdatePopulationPlus(pop, newpop);
+                // Decide if we need to restart due to stagnation
+                if (pop.hasConverged()) {
+                    pop = RestartPopulation(pop, preservePercent);
+                }
+            } while (!TerminationCriterion());
+
+            //TODO
+            // Output result
+            List<Integer>[] popPheno = ConvertPopToPhenotype(pop);
+            int bestSolution = FindBestSolution(popPheno);
+            System.out.println("Genotype: " + pop.getSolution(bestSolution).toString());
+            System.out.println("Phenotype: " + popPheno[bestSolution].toString());
+            System.out.println("Score: " + Evaluate(popPheno[bestSolution]));
+
+        } catch (NumberFormatException e) {
+            //TODO
+            e.printStackTrace();
+        }
     }
 }
