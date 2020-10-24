07b770ee6d1aa826a7372615ccaa14ac8e2ecf93
Radcliffe, M <meleena.radcliffe@gmail.com>
2020-10-25 10:57:24 +1300

    Allowed multiple runs to be carried out

--- src/Main.java
+++ src/Main.java
@@ -80,169 +80,188 @@ public class Main {
         return search.getResult();
     }
 
+/**Usage: [filePath] [maxDegree] -S [randomSeed] -Pop [popSize] -parents [parentSize]
+ * -preserve [percentagePreserved] -mutate [mutationProbability] {-plus, -comma} {-replicated, -synchronous
+ * -tMax [maxRunTime]");
+**/
+
+public static void doRuns(String[] args){
+    // Set default parameters
+    int n_pr =  Runtime.getRuntime().availableProcessors();
+    int popSize = 8;
+    int numParents = 2;
+    int numChildren = 4; // TODO Allow the number of children to be set
+    int maxConnection;
+    double preservePercent = 0.2;
+    double mutatePercent = 0.2;
+    boolean sectionInheritance = true;
+    boolean plusInsteadOfComma = true;
+    boolean replicatedInsteadOfSynchronous = true;
+    long tMax = 1;
+    long localtMax = (long) 0.01;
+    int kMax = 10;
+    Random rand = new Random();
+
+    try {
+        if(args.length == 0){
+            System.out.println("Usage: [filePath] [maxDegree] -S [randomSeed] -Pop [popSize] -parents [parentSize]" +
+                    " -preserve [percentagePreserved] -mutate [mutationProbability] {-plus, -comma} {-replicated, -synchronous} -tMax [maxRunTime]");
+        }
+        // Read in the parameters
+        if (args.length > 1) {
+            // First argument should be the filepath of the lengths to connect each node in a specific csv format
+            String filePath = args[0];
+            readCSV(filePath);
+
+            // Second argument should be the max number of connections for each node
+            maxConnection = Integer.parseInt(args[1]);
+
+            if (args.length > 2) {
+                for (int i = 2; i < args.length; i++) {
+                    // Optional -S argument followed by an integer is the number for the random seed
+                    if (args[i].equals("-S")) {
+                        int seed = Integer.parseInt(args[i + 1]);
+                        rand = new Random(seed);
+                        i++;
+                    }
+                    // Optional -Pop argument followed by an integer is the population size
+                    else if (args[i].equals("-Pop")) {
+                        popSize = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+
+                    // Optional -parents argument followed by an integer is the number of parents
+                    else if (args[i].equals("-parents")) {
+                        numParents = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    // Optional -preserve argument followed by a double is the preserve Percent
+                    else if (args[i].equals("-preserve")) {
+                        preservePercent = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    // Optional -mutate argument followed by a double is the mutatePercent
+                    else if (args[i].equals("-mutate")) {
+                        mutatePercent = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    // Optional argument of a -plus or -comma keyword to select the recombination method
+                    else if (args[i].equals("-plus") ) {
+                        plusInsteadOfComma = true;
+                    }
+                    else if (args[i].equals("-comma") ) {
+                        plusInsteadOfComma = true;
+                    }
+                    // Optional argument of -replicated or -synchronous to select the parallelism method and
+                    //  followed by an integer to choose the number cores to use
+                    else if (args[i].equals("-replicated") ) {
+                        replicatedInsteadOfSynchronous = true;
+                    }
+                    else if (args[i].equals("-synchronous") ) {
+                        replicatedInsteadOfSynchronous = false;
+                    }
+                    // Optional argument of -kMax followed by a long is the max time to search
+                    else if (args[i].equals("-kMax")) {
+                        kMax = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    // Optional argument of -tMax followed by a long is the max time to search
+                    else if (args[i].equals("-tMax")) {
+                        tMax = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                    // Optional argument of -localtMax followed by a long is the max time to do local search
+                    else if (args[i].equals("-localtMax")) {
+                        localtMax = Integer.parseInt(args[i + 1]);
+                        i++;
+                    }
+                }
+            }
+            // Start the timer to record how long it takes to search for the best solution.
+            final long initialTime = System.nanoTime();
+
+            Genotype bestSolution = null;
+            List<Integer> solutionPhenotype = null;
+            int bestScore = 0;
+
+            // Start Replicated Parallel Search
+            if (replicatedInsteadOfSynchronous) {
+                Search[] searches = new Search[n_pr];
+                for (int i = 0; i < n_pr; i++) {
+                    searches[i] = new Search(rand, popSize, numParents, numChildren, numberOfNodes, numberOfUniqueLinks, maxConnection,
+                            linkLengths, mutatePercent, preservePercent, sectionInheritance, plusInsteadOfComma, tMax, localtMax, kMax);
+                }
+
+                List<Callable<Genotype>> tasks = new ArrayList<Callable<Genotype>>();
+                for (final Search search : searches) {
+                    Callable<Genotype> callable = new Callable<Genotype>() {
+                        @Override
+                        public Genotype call() throws Exception {
+                            return compute(search);
+                        }
+                    };
+                    tasks.add(callable);
+                }
+
+                ExecutorService exec = Executors.newFixedThreadPool(n_pr);
+                // some other executors we could try to see the different behaviours
+                // ExecutorService exec = Executors.newCachedThreadPool();
+                // ExecutorService exec = Executors.newSingleThreadExecutor();
+
+                try {
+                    // Start all searches to run in parallel
+                    List<Future<Genotype>> results = exec.invokeAll(tasks);
+                    // Find best solution of all searches
+                    for (Future<Genotype> search : results) {
+
+                        // Wait until all threads have finished execution and get best solution
+                        Genotype newSolution = search.get();
+                        // Convert to phenotype space to then evaluate
+                        List<Integer> newPheno = Search.Growth(newSolution);
+                        int newScore = Search.Evaluate(newPheno);
+                        // Keep track of the solution with the best score
+                        if (bestSolution == null || newScore < bestScore) {
+                            bestSolution = newSolution;
+                            solutionPhenotype = newPheno;
+                            bestScore = newScore;
+                        }
+                    }
+                } finally {
+                    exec.shutdown();
+                }
+
+            } else {
+                Search search = new Search(rand, popSize, numParents, numChildren, numberOfNodes, numberOfUniqueLinks, maxConnection,
+                        linkLengths, mutatePercent, preservePercent, sectionInheritance, plusInsteadOfComma, tMax, localtMax, kMax);
+                search.run();
+                search.join(0);
+                bestSolution = search.getResult();
+                solutionPhenotype = search.Growth(bestSolution);
+                bestScore = search.Evaluate(solutionPhenotype);
+            }
+            final double secondsToComplete = (System.nanoTime() - initialTime) / 1E9;
+
+            System.out.println("Completed in " + secondsToComplete + " seconds");
+            System.out.println("Genotype: " + bestSolution.toString());
+            System.out.println("Phenotype: " + solutionPhenotype.toString());
+            System.out.println(bestSolution.idLinks(linkIDs));
+            System.out.println("Score: " + bestScore);
+        }
+
+    } catch (NumberFormatException | InterruptedException | ExecutionException e) {
+        //TODO
+        e.printStackTrace();
+    }
+
+}
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        // Set default parameters
-        int n_pr =  Runtime.getRuntime().availableProcessors();
-        int popSize = 8;
-        int numParents = 2;
-        int numChildren = 4; // TODO Allow the number of children to be set
-        int maxConnection;
-        double preservePercent = 0.2;
-        double mutatePercent = 0.2;
-        boolean sectionInheritance = true;
-        boolean plusInsteadOfComma = true;
-        boolean replicatedInsteadOfSynchronous = true;
-        long tMax = 1;
-        long localtMax = (long) 0.01;
-        int kMax = 10;
-        Random rand = new Random();
-
-        try {
-            // Read in the parameters
-            if (args.length > 1) {
-                // First argument should be the filepath of the lengths to connect each node in a specific csv format
-                String filePath = args[0];
-                readCSV(filePath);
-
-                // Second argument should be the max number of connections for each node
-                maxConnection = Integer.parseInt(args[1]);
-
-                if (args.length > 2) {
-                    for (int i = 2; i < args.length; i++) {
-                        // Optional -S argument followed by an integer is the number for the random seed
-                        if (args[i].equals("-S")) {
-                            int seed = Integer.parseInt(args[i + 1]);
-                            rand = new Random(seed);
-                            i++;
-                        }
-                        // Optional -Pop argument followed by an integer is the population size
-                        else if (args[i].equals("-Pop")) {
-                            popSize = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-
-                        // Optional -parents argument followed by an integer is the number of parents
-                        else if (args[i].equals("-parents")) {
-                            numParents = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                        // Optional -preserve argument followed by a double is the preserve Percent
-                        else if (args[i].equals("-preserve")) {
-                            preservePercent = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                        // Optional -mutate argument followed by a double is the mutatePercent
-                        else if (args[i].equals("-mutate")) {
-                            mutatePercent = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                        // Optional argument of a -plus or -comma keyword to select the recombination method
-                        else if (args[i].equals("-plus") ) {
-                            plusInsteadOfComma = true;
-                        }
-                        else if (args[i].equals("-comma") ) {
-                            plusInsteadOfComma = true;
-                        }
-                        // Optional argument of -replicated or -synchronous to select the parallelism method and
-                        //  followed by an integer to choose the number cores to use
-                        else if (args[i].equals("-replicated") ) {
-                            replicatedInsteadOfSynchronous = true;
-                        }
-                        else if (args[i].equals("-synchronous") ) {
-                            replicatedInsteadOfSynchronous = false;
-                        }
-                        // Optional argument of -kMax followed by a long is the max time to search
-                        else if (args[i].equals("-kMax")) {
-                            kMax = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                        // Optional argument of -tMax followed by a long is the max time to search
-                        else if (args[i].equals("-tMax")) {
-                            tMax = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                        // Optional argument of -localtMax followed by a long is the max time to do local search
-                        else if (args[i].equals("-localtMax")) {
-                            localtMax = Integer.parseInt(args[i + 1]);
-                            i++;
-                        }
-                    }
-                }
-                // Start the timer to record how long it takes to search for the best solution.
-                final long initialTime = System.nanoTime();
-
-                Genotype bestSolution = null;
-                List<Integer> solutionPhenotype = null;
-                int bestScore = 0;
-
-                // Start Replicated Parallel Search
-                if (replicatedInsteadOfSynchronous) {
-                    Search[] searches = new Search[n_pr];
-                    for (int i = 0; i < n_pr; i++) {
-                        searches[i] = new Search(rand, popSize, numParents, numChildren, numberOfNodes, numberOfUniqueLinks, maxConnection,
-                                linkLengths, mutatePercent, preservePercent, sectionInheritance, plusInsteadOfComma, tMax, localtMax, kMax);
-                    }
-
-                    List<Callable<Genotype>> tasks = new ArrayList<Callable<Genotype>>();
-                    for (final Search search : searches) {
-                        Callable<Genotype> callable = new Callable<Genotype>() {
-                            @Override
-                            public Genotype call() throws Exception {
-                                return compute(search);
-                            }
-                        };
-                        tasks.add(callable);
-                    }
-
-                    ExecutorService exec = Executors.newFixedThreadPool(n_pr);
-                    // some other executors we could try to see the different behaviours
-                    // ExecutorService exec = Executors.newCachedThreadPool();
-                    // ExecutorService exec = Executors.newSingleThreadExecutor();
-
-                    try {
-                        // Start all searches to run in parallel
-                        List<Future<Genotype>> results = exec.invokeAll(tasks);
-                        // Find best solution of all searches
-                        for (Future<Genotype> search : results) {
-
-                            // Wait until all threads have finished execution and get best solution
-                            Genotype newSolution = search.get();
-                            // Convert to phenotype space to then evaluate
-                            List<Integer> newPheno = Search.Growth(newSolution);
-                            int newScore = Search.Evaluate(newPheno);
-                            // Keep track of the solution with the best score
-                            if (bestSolution == null || newScore < bestScore) {
-                                bestSolution = newSolution;
-                                solutionPhenotype = newPheno;
-                                bestScore = newScore;
-                            }
-                        }
-                    } finally {
-                        exec.shutdown();
-                    }
-
-                } else {
-                    Search search = new Search(rand, popSize, numParents, numChildren, numberOfNodes, numberOfUniqueLinks, maxConnection,
-                            linkLengths, mutatePercent, preservePercent, sectionInheritance, plusInsteadOfComma, tMax, localtMax, kMax);
-                    search.run();
-                    search.join(0);
-                    bestSolution = search.getResult();
-                    solutionPhenotype = search.Growth(bestSolution);
-                    bestScore = search.Evaluate(solutionPhenotype);
-                }
-                final double secondsToComplete = (System.nanoTime() - initialTime) / 1E9;
-
-                System.out.println("Completed in " + secondsToComplete + " seconds");
-                System.out.println("Genotype: " + bestSolution.toString());
-                System.out.println("Phenotype: " + solutionPhenotype.toString());
-                System.out.println(bestSolution.idLinks(linkIDs));
-                System.out.println("Score: " + bestScore);
-            }
-
-        } catch (NumberFormatException | InterruptedException | ExecutionException e) {
-            //TODO
-            e.printStackTrace();
+    int numRuns = 10;
+        for(int i = 0; i < numRuns; i++){
+            System.out.println("=======================");
+            System.out.println("Run number " + (i+1) + ":");
+            System.out.println("=======================");
+            doRuns(args);
         }
     }
 }
